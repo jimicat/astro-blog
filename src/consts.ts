a1877817268007f9ef60549c657023d7abc1df8a
tech1024 <941104c@gmail.com>
2024-12-02 14:21:40 +0800

    update

---
+++ src/consts.ts
@@ -0,0 +1,33 @@
+import type { Metadata, Site, Socials } from "@types";
+
+export const SITE: Site = {
+  TITLE: "陈迪の自留地",
+  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
+  EMAIL: "941104c@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "Projects",
+  DESCRIPTION:
+    "A collection of my projects with links to repositories and live demos.",
+};
+
+export const SOCIALS: Socials = [
+
+  {
+    NAME: "GitHub",
+    HREF: "https://github.com/jimicat",
+  },
+];
