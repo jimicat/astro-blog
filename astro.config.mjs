6022b5adbb04e404c69e846699d42636e3bb5874
tech1024 <941104c@gmail.com>
2024-12-02 14:36:28 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jimicat.github.io",
-  base: "/",
+  base: "/astro-blog",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
